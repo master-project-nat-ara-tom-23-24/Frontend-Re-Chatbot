9e47546095437963fd340e186adbb14f50c5ad9c
tlrmendonca <tomas.mendonca.01@gmail.com>
2024-02-06 16:23:04 +0100

    ACCESS AI visual adjustments and chat log - chat is not actually saved yet, but can be displayed

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { useChatbot } from './Hooks'
+import { set } from 'lodash'
 
 // interface ChatbotProps {
 //     taskInfo: TaskInfoI,
@@ -21,42 +22,43 @@ interface MessageI {
 
 export const Chatbot = () => {
     const [inputText, setInputText] = useState<string>('');
+    const [MessageArray, setMessageArray] = useState<Array<MessageI>>([]);
     const { data: query, submit, timer } = useChatbot('123')
+    const {} = useParams()
     // const chatbotAPI = axios.create({
     //     baseURL: `http://localhost:8081/${ctx.courseSlug}/assignments/${ctx.assignmentID}/tasks/${ctx.taskID}/chat/123/users/123/prompt`
     // }); ///{courseSlug}/assignments/{assignment}/tasks/{task}/chat/{chat}/users/{user}/prompt
     // chatbotAPI.defaults.headers.post['Content-Type'] = 'application/json';
     // chatbotAPI.defaults.headers.common = axios.defaults.headers.common;
 
-    var MessagesArray : Array<MessageI> = [
-        {prompt: "Hello", answer: "Hi there!"},
-        {prompt: "How are you?", answer: "I'm good, thanks!"},
-        {prompt: "What's your name?", answer: "My name is Chatbot."}
-    ]
+    const proccessResponse = (response: string) => {
+        setMessageArray([...MessageArray, {prompt: inputText, answer: response}]);
+        setInputText(''); // Reseting input
+    }
 
     const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
-            // postRequest();
-            var answer = await submit({ chatId: null, prompt: inputText })
-            console.log('ANSWER ' + answer)
-            // setDisplayText('My input: ' + inputText + '. To URL: ' + chatbotAPI.defaults.baseURL); // Debug pruposes
-            setInputText(''); // Reseting input
+            // var answer = await submit({ chatId: null, prompt: inputText })
+            var answer = 'Hello! I am ACCESS AI and this is an example of a response.' 
+            proccessResponse(answer);
         }
     };
 
     return (
         <div id="chat-container" style={{fontFamily: "Arial, sans-serif"}}>
             <div id="message-area" style={{color: "#fff", padding: "10px"}}>
-                {!MessagesArray.length ?
+                {!MessageArray.length ?
                 null
                 :
-                MessagesArray.map((message, index) => (
+                MessageArray.map((message, index) => (
                     <div key={index} style={{marginBottom: "10px"}}>
-                        <Box marginBottom="5px" borderRadius="md" backgroundColor="gray.200" borderColor="gray.300" borderWidth="1px" paddingLeft={"10px"}>
-                            <Text color="#fff">{'User : ' + message.prompt}</Text>
+                        <Text color="#fff">{'You'}</Text>
+                        <Box marginBottom="8px" borderRadius="md" backgroundColor="gray.200" borderColor="gray.300" borderWidth="1px" paddingLeft={"10px"}>
+                            <Text color="#fff">{message.prompt}</Text>
                         </Box>
-                        <Box marginBottom="15px" borderRadius="md" backgroundColor="gray.200" borderColor="gray.300" borderWidth="1px" paddingLeft={"10px"}>
-                            <Text color="#ffa500">{'Bot ^^ : ' + message.answer}</Text>
+                        <Text color="#ffa500">{'ACCESS AI'}</Text>
+                        <Box marginBottom="12px" borderRadius="md" backgroundColor="gray.200" borderColor="gray.300" borderWidth="1px" paddingLeft={"10px"}>
+                            <Text color="#ffa500">{message.answer}</Text>
                         </Box>
                     </div>
                 ))}
