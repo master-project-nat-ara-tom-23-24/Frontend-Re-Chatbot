b93c5bfae8613a1c9eb43029d34696be4fea8c9a
tlrmendonca <tomas.mendonca.01@gmail.com>
2024-03-06 16:14:11 +0100

    Added status rendering and dummy data for now

--- src/components/chatbot/FileUploadStatus.tsx
+++ src/components/chatbot/FileUploadStatus.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, VStack, Icon } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query'
 import { useStatus } from '../Hooks';
+import { FiSend } from 'react-icons/fi'
 
 
 export const FileUploadStatus = () => {
     const { data: courses } = useQuery<CourseOverview[]>(['courses'])
     const query = useStatus();
-    const [statusArray, setStatusArray] = useState<Array<FilesUploadStatusI | undefined>>([]);
+    const [statusArray, setStatusArray] = useState<Array<CourseFilesUploadStatusI | undefined>>([]);
     const [time, setTime] = useState<number>(0);
 
     useEffect(() => {
+        fillStatusDummyData();
         // todo
     }, []);
 
@@ -25,39 +27,82 @@ export const FileUploadStatus = () => {
     }
 
     const fillStatusDummyData = () => {
-        // create 3 dummy status objects
-        let status1: FilesUploadStatusI = {
+        let statusGreen: FilesUploadStatusI = {
+            successful: ["This is a successful message"],
+            failed: [],
+            date: new Date()
+        };
+        let statusYellow: FilesUploadStatusI = {
             successful: ["This is a successful message"],
             failed: ["This is a failed message"],
             date: new Date()
         };
-        let status2: FilesUploadStatusI = {
-            successful: ["This is a successful message"],
+        let statusRed: FilesUploadStatusI = {
+            successful: [],
             failed: ["This is a failed message"],
             date: new Date()
         };
-        let status3: FilesUploadStatusI = {
-            successful: ["This is a successful message"],
-            failed: ["This is a failed message"],
-            date: new Date()
-        };
+        let courseStatusArray: Array<CourseFilesUploadStatusI> = [];
+
+        if (courses?.length === 0) {
+            // 3 course status
+            const courseSlugs = ["dummy-course1", "dummy-course2", "dummy-course3"];
+            const statusColors = [statusGreen, statusYellow, statusRed];
+            courseSlugs.forEach((slug, index) => {
+                let courseStatus: CourseFilesUploadStatusI = {
+                    courseSlug: slug,
+                    status: statusColors[index % statusColors.length]
+                };
+                courseStatusArray.push(courseStatus);
+            });
+        } else {
+            for (var course in courses) {
+                courseStatusArray.push({ courseSlug: course, status: statusGreen });
+            }
+        }
+        setStatusArray(courseStatusArray);
     };
 
-    interface BoxProps {
-        // define your props here if any
+    const getColor = (success: string[], failed : string[]) : string => {
+        let color : string;
+
+        if (failed.length === 0) {
+            color = 'green';
+        } else if (failed.length != 0 && success.length != 0) {
+            color = 'yellow';
+        } else 
+            color = 'red';
+
+        return color;
     }
 
-    if (!courses)
-        return <></>
+    // if (!courses || courses.length === 0)
+    //     return <></>
     
     return (
-        <Box>
+        <VStack justify='center' spacing={4} minH='xs' color='blackAlpha.800'>
+            <Icon as={FiSend} boxSize={16} opacity={0.3} />
             {statusArray.map((status, index) => {
-                return (
-                    <Text key={index}>{status?.date.toString()}</Text>
+                if (status) {
+                    let color = getColor(status.status.successful, status.status.failed)
+                    return (
+                    <Box border="2px solid" padding="8px 12px" bg={color + '.200'} borderColor={color + '.500'} borderRadius="8px" width='90%'>
+                        <Text>{status.courseSlug}</Text>
+                        <Text>{status.status.successful}</Text>
+                    </Box>
                     );
+                }
+                else return (<></>);
                 })
             }
-        </Box>
+        </VStack>
+        // <Box>
+            // {statusArray.map((status, index) => {
+            //     return (
+            //         <Text>{status?.courseSlug}</Text>
+            //         );
+            //     })
+            // }
+        // </Box>
     );
 }
